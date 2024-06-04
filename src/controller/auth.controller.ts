a284564ca77d57b4b344d634ea4502bf3a439d73
peppapig13132 <peppapig13132@outlook.com>
2024-06-04 17:43:07 +0200

    Encrypt user password to store in database

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,17 +1,23 @@
 import { Request, Response, RequestHandler } from 'express';
 import asyncHandler from 'express-async-handler';
 import User from '../model/user.model';
+import bcrypt from 'bcrypt';
 
 export const signup: RequestHandler = asyncHandler(async (req: Request, res: Response) => {
   const u = req.body;
 
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(u.password, saltRounds);
+
   const user = await User.create({
     email: u.email,
-    password: u.password,
+    password: passwordHash,
   })
 
   res.json({
     ok: true,
-    user: user,
+    user: {
+      user_id: user.dataValues.id,
+    },
   });
 });
